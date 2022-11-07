672e367ad56f9b7aa91b9d2fdad715a32de8d194
Aufa Fauqi Ardhiqi <79738424+ardhiqii@users.noreply.github.com>
2022-11-07 13:23:44 +0700

    Tugas 2

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, jsonify, request
 import json
 # Intitialise the app
 app = Flask(__name__)
+# Bikin jsonify sorted sesuai dengan code
+app.config['JSON_SORT_KEYS'] = False
 
 def read_json(filename="data.json"):
     with open(filename,"r") as read_file:
@@ -23,6 +25,10 @@ def index():
 @app.get("/editName")
 def editName():
     name = request.args.get("name")
+    if not name:
+        return jsonify({"status":"error","deskripsi":"parameter salah atau input name kosong"
+        })
+    
     write_json(name)
     data = read_json()
     return jsonify(data)

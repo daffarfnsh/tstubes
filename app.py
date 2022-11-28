583847d6362d2ad1fb1a3b1b74acc2f0c3f08bc8
Dito <ammarditoshafaat2001@gmail.com>
2022-11-29 03:27:00 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,20 @@
 from flask import Flask, jsonify, request
 import json
 from flask_mysqldb import MySQL
+
 # Intitialise the app
 app = Flask(__name__)
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'testtst'
+app.config['MYSQL_DB'] = 'tubestst'
 mysql = MySQL(app)
-
-
-# print(data)
-
 
 # Define what the app does
 @app.get("/")
 def index():
     cur = mysql.connection.cursor()
-    table = 'estimated_crimes_1979_2019'
+    table = 'house'
     cur.execute(f'''select * from {table} limit 5''')
     data = cur.fetchall()
     return jsonify(data)

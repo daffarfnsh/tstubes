fce01a9209439404dec08894ea97adea001e3244
daffarfnsh <18220006@std.stei.itb.ac.id>
2022-11-29 04:33:51 +0700

    tugas 5 db

--- app.py
+++ app.py
@@ -7,14 +7,14 @@ app = Flask(__name__)
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'tubestst'
+app.config['MYSQL_DB'] = 'testst'
 mysql = MySQL(app)
 
 # Define what the app does
 @app.get("/")
 def index():
     cur = mysql.connection.cursor()
-    table = 'house'
-    cur.execute(f'''select * from {table} limit 5''')
+    table = 'linkedin'
+    cur.execute(f'''select * from {table} limit 1''')
     data = cur.fetchall()
     return jsonify(data)

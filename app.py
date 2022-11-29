8baa40a48c048f3d242c293fe0acbd8a6adcf189
daffarfnsh <73842427+daffarfnsh@users.noreply.github.com>
2022-11-30 02:40:14 +0700

    last update tgs 6

--- app.py
+++ app.py
@@ -15,9 +15,9 @@ table = 'linkedin'
 @app.route("/",methods=['GET','POST'])
 def index():
     cur = mysql.connection.cursor()
-    linkedin = 'www.linkedin.com'
-    query = f'select * from {table} limit 1'
-    # query = f'select * from {table} where linkedin = "{linkedin}"'
+    # query = f'select * from {table} limit 3'
+    profile_picture = 'Dummy1'
+    query = f'select * from {table} where profile_picture = "{profile_picture}"'
     cur.execute(query)
     data = cur.fetchall()
     return jsonify(data)
@@ -25,28 +25,25 @@ def index():
 @app.route("/create",methods=['GET','POST'])
 def create():
     cur = mysql.connection.cursor()
-    index = 2022
-    category = 'CS'
-    linkedin = 'www.linkedin.com'
-    query = f'insert into {table} values ({index},"{category}","{linkedin}","a","b","c","d","e","f","g","h")'
+    query = f'insert into {table} values (2022,"CS","in.linkedin","Dummy","Dummy","Dummy","Dummy","Dummy","Dummy","Dummy","Dummy")'
     cur.execute(query)
     mysql.connection.commit()
     return 'data created'
 @app.route('/update',methods=['GET','POST','PUT'])
 def update():
     cur = mysql.connection.cursor()
-    index = 2022
     category = 'CS'
-    linkedin = 'www.linkedin.com/'
-    query = f'update {table} set linkedin = "{linkedin}" where index = {index} and category ="{category}"'
+    linkedin = 'in.linkedin'
+    profile_picture = 'Dummy1'
+    query = f'update {table} set profile_picture = "{profile_picture}" where linkedin ="{linkedin}"'
     cur.execute(query)
     mysql.connection.commit()
     return 'data updated'
 @app.route('/delete',methods=['GET','POST','DELETE'])
 def delete():
     cur = mysql.connection.cursor()
-    linkedin = 'www.linkedin.com'
-    query = f'delete from {table} where linkedin = "{linkedin}"'
+    profile_picture = 'Dummy1'
+    query = f'delete from {table} where profile_picture = "{profile_picture}"'
     cur.execute(query)
     mysql.connection.commit()
     return 'data deleted'
